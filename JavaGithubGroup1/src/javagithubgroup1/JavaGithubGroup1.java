a1cabcb6f13a74e5d6ae76e53372c58474e6507b
Mona-Almalki <144262699+Mona-Almalki@users.noreply.github.com>
2023-09-13 21:38:44 +0300

    Update JavaGithubGroup1.java

--- JavaGithubGroup1/src/javagithubgroup1/JavaGithubGroup1.java
+++ JavaGithubGroup1/src/javagithubgroup1/JavaGithubGroup1.java
@@ -18,7 +18,6 @@ public class JavaGithubGroup1 {
         System.out.println("Mona");
         System.out.println("Reema");
         System.out.println("Bakerah");
-        System.out.println("..");
 
         
     }
