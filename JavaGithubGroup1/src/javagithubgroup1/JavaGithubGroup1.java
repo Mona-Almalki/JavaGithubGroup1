57fff5615692e4aae7e2174fa1d17ac0b170aa49
Mona <Mona@DESKTOP-F7P55S6>
2023-09-13 18:59:17 +0300

    first commit!

---
+++ JavaGithubGroup1/src/javagithubgroup1/JavaGithubGroup1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javagithubgroup1;
+
+/**
+ *
+ * @author Mona
+ */
+public class JavaGithubGroup1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Group1");
+    }
+    
+}
