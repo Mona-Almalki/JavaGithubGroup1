9acf591c6f84fa17d27c050b41241c00610c44e5
Mona <Mona@DESKTOP-F7P55S6>
2023-10-29 23:10:26 +0300

    first commit!

--- Group1Project/GetAlongWithMeCommunity/src/getalongwithmecommunity/GetAlongWithMeCommunity.java
+++ Group1Project/GetAlongWithMeCommunity/src/getalongwithmecommunity/GetAlongWithMeCommunity.java
@@ -8,14 +8,14 @@ public class GetAlongWithMeCommunity {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        //volunteer registration function 
+    public static void main(String[] args) { 
         LinkedList volunteerList = new LinkedList();
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            System.out.println("\n                     Volunteer Registration                     ");
-            System.out.println("__________________________________________________________________");
+            System.out.println("\n                Welcome to Get Along with me community                ");
+            System.out.println("________________________________________________________________________");
+            System.out.println("Services:");
             System.out.println("1. Register a Volunteer");
             System.out.println("2. Exit");
             System.out.print("\nEnter your choice: ");
@@ -23,34 +23,42 @@ public class GetAlongWithMeCommunity {
             
 
             int choice = scanner.nextInt();
-            scanner.nextLine(); // Consume the newline character
+            scanner.nextLine();
 
             switch (choice) {
                 case 1:
-                    System.out.print("Enter Your Name: ");
-                    String name = scanner.nextLine();
-                    System.out.print("Enter Your Age: ");
-                    int age = scanner.nextInt();
-                    scanner.nextLine();
-                    System.out.print("Enter Your Phone Number: ");
-                    String phone = scanner.nextLine();
-                    System.out.print("Enter Your Email: ");
-                    String email = scanner.nextLine();
-                    System.out.print("Enter Your City: ");
-                    String city = scanner.nextLine();
-
-                    volunteerList.append(name, age, phone, email, city);
-                    System.out.println("Volunteer registered successfully!");
+                    registerVolunteer(scanner, volunteerList);
                     break;
                 case 2:
-                    System.out.println("Exiting Volunteer Registration System. Goodbye!");
+                    System.out.println("Exiting Get Along with me community System. Goodbye!");
                     scanner.close();
                     System.exit(0);
                 default:
                     System.out.println("Invalid choice. Please select a valid option.");
             }
+            
+            
         }
+        
 
+    }
+    
+    ////volunteer registration function
+    public static void registerVolunteer(java.util.Scanner scanner, LinkedList volunteerList) {
+        System.out.print("Enter Your Name: ");
+        String name = scanner.nextLine();
+        System.out.print("Enter Your Age: ");
+        int age = scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("Enter Your Phone Number: ");
+        String phone = scanner.nextLine();
+        System.out.print("Enter Your Email Address: ");
+        String email = scanner.nextLine();
+        System.out.print("Enter Your City: ");
+        String city = scanner.nextLine();
+
+        volunteerList.append(name, age, phone, email, city);
+        System.out.println("Volunteer registered successfully!");
     }
     
 }
