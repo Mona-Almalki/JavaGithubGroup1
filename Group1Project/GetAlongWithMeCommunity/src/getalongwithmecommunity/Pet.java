4c38c61ba447869d96a3952c7a9f61b163211b94
Bakerah-Alseari <143954286+Bakerah-Alseari@users.noreply.github.com>
2023-11-02 21:26:09 +0300

    Create Pet.java

---
+++ Group1Project/GetAlongWithMeCommunity/src/getalongwithmecommunity/Pet.java
@@ -0,0 +1,65 @@
+public class Pet {
+    private int petId;
+    private String name;
+    private String type;
+    private int age;
+    private boolean isApproved = false;
+
+    public Pet(int petId, String name, String type, int age) {
+        this.petId = petId;
+        this.name = name;
+        this.type = type;
+        this.age = age;
+    }
+
+    public void setPetId(int petId) {
+        this.petId = petId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    public void Approve(){
+        isApproved=true;
+    }
+    
+    
+
+    public int getPetId() {
+        return petId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public boolean isIsApproved() {
+        return isApproved;
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+}
