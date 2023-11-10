5c3a4385d0da0aa84223854e84d57fb8ce6116cd
Mona-Almalki <144262699+Mona-Almalki@users.noreply.github.com>
2023-11-10 21:21:30 +0300

    Update JavaGithubGroup1.java

--- JavaGithubGroup1/src/javagithubgroup1/JavaGithubGroup1.java
+++ JavaGithubGroup1/src/javagithubgroup1/JavaGithubGroup1.java
@@ -14,7 +14,6 @@ public class JavaGithubGroup1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Group1");
         System.out.println("Mona");
         System.out.println("Reema");
         System.out.println("Bakerah");
