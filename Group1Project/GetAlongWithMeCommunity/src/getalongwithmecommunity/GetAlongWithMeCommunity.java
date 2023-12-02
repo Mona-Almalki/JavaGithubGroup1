3632e1602e29b132a63f358cf0eda424876b5556
Jumana-albeladi <142648947+jumana-Albeladi@users.noreply.github.com>
2023-12-03 00:55:36 +0300

    Update GetAlongWithMeCommunity.java
    I have edited a few methodes

--- Group1Project/GetAlongWithMeCommunity/src/getalongwithmecommunity/GetAlongWithMeCommunity.java
+++ Group1Project/GetAlongWithMeCommunity/src/getalongwithmecommunity/GetAlongWithMeCommunity.java
@@ -206,13 +206,54 @@ public class GetAlongWithMeCommunity {
         } 
     }
 
-     public static void adopt(Scanner scanner) {
-    System.out.println("\n********** Adopt a Pet **********");
+     System.out.println("\n********** Adopt a Pet **********");
 
     System.out.print("Enter the pet ID: ");
     int petId = getValidIntInput(scanner);
 
-    // Find the pet with the given ID
+    // Check if the pet with the given ID exists
+    if (!petExists(petId)) {
+        System.out.println("Pet with ID " + petId + " not found.");
+        return;
+    }
+
+    // Read the entire file and store its contents
+    ArrayList<String> fileContents = new ArrayList<>();
+    try (BufferedReader reader = new BufferedReader(new FileReader(FILE_NAME))) {
+        String line;
+        while ((line = reader.readLine()) != null) {
+            String[] petInfo = line.split(",");
+            if (petInfo.length >= 1) {
+                String[] petIdInfo = petInfo[0].split("=");
+                if (petIdInfo.length >= 2) {
+                    int currentPetId = Integer.parseInt(petIdInfo[1]);
+
+                    // Check if the line corresponds to the pet being adopted
+                    if (currentPetId == petId) {
+                        continue; // Skip the line to exclude the adopted pet
+                    }
+                }
+            }
+
+            // Add the line to the file contents
+            fileContents.add(line);
+        }
+    } catch (IOException ex) {
+        System.out.println("Error in reading from file");
+        return;
+    }
+
+    // Update the file with modified pet information
+    try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_NAME))) {
+        for (String line : fileContents) {
+            writer.write(line);
+            writer.newLine();
+        }
+    } catch (IOException ex) {
+        System.out.println("Error in writing to file");
+    }
+
+    // Remove the pet from the list
     Pet petToRemove = null;
     for (Pet pet : petList) {
         if (pet.getPetId() == petId) {
@@ -221,60 +262,23 @@ public class GetAlongWithMeCommunity {
         }
     }
 
-    // Remove the pet from the list
     if (petToRemove != null) {
         petList.remove(petToRemove);
         System.out.println("Pet with ID " + petId + " adopted successfully!");
-
-        // Read the entire file and store its contents
-        ArrayList<String> fileContents = new ArrayList<>();
-        try (BufferedReader reader = new BufferedReader(new FileReader(FILE_NAME))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                if (!line.startsWith("Volunteer")) { // Exclude volunteer lines
-                    String[] petInfo = line.split(",");
-                    int currentPetId = Integer.parseInt(petInfo[0].split("=")[1]);
-
-                    // Check if the line corresponds to the pet being adopted
-                    if (currentPetId == petId) {
-                        continue; // Skip the line to exclude the adopted pet
-                    }
-                }
-
-                // Add the line to the file contents
-                fileContents.add(line);
-            }
-        } catch (IOException ex) {
-            System.out.println("Error in reading from file");
-            return;
-        }
-
-        // Update the file with modified pet information
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_NAME))) {
-            for (String line : fileContents) {
-                writer.write(line);
-                writer.newLine();
-            }
-            System.out.println("Pet information updated successfully!");
-        } catch (IOException ex) {
-            System.out.println("Error in writing to file");
-        }
     } else {
         System.out.println("Pet with ID " + petId + " not found.");
     }
 }
-    
-    
-    
-    // check if pet exist
-    private static boolean petExists(int id) {
-        for (Pet pet: petList) 
-           if(pet.getPetId()==id) 
-               return true;
-        return false;
-               
+
+// Check if pet exists
+private static boolean petExists(int id) {
+    for (Pet pet : petList) {
+        if (pet.getPetId() == id) {
+            return true;
+        }
     }
-    
+    return false;
+}
     
     // check int input
     private static int getValidIntInput(Scanner scanner) {
